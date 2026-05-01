f746633df91038433aaa5d4e075a3c9408b8cd1a
Thejesh224 <pthejesh27@gmail.com>
2026-05-01 09:53:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage, AIMessage
 
