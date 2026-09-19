497ca93594ed5719405a61bf25a115decd80c997
Thejesh224 <pthejesh27@gmail.com>
2026-09-19 22:16:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from bs4 import BeautifulSoup
 from docx import Document as DocxDocument
 
 from langchain_core.documents import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
